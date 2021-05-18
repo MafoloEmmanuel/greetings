044c6b50c90093dfad51ab86f4247ed447b3730a
mafoloEmmanuel <mafoloemmanuel@gmail.com>
2021-05-19 01:41:23 +0200

    few changes

--- greet-factory.js
+++ greet-factory.js
@@ -1,10 +1,34 @@
 function theGreetExercise(){
+/*
 
-    function greet(name){
+    function greetEnglish(name){
     return 'Hello, '+ name;
+    }
+    function greetSepedi(name){
+        return 'Dumela, '+ name;
+        }
+    function greetZulu(name){
+            return 'Molo, '+ name;
+            }
+
+    function greetingsLanguage(myLanguage){
+        if(myLanguage=== "English"){
+            return greetEnglish();
+        } else if(myLanguage=== "Sepedi"){
+            return greetSepedi();
+        } else if(myLanguage=== "Zulu"){
+            return greetZulu();
+        }
+    }*/
+    function greet(name){
+        return 'Hello, ' + name;
     }
     return {
         greet,
+       // greetEnglish,
+       // greetSepedi,
+      //  greetZulu,
+      // greetingsLanguage
     }
 
 }
--- greet.js
+++ greet.js
@@ -1,11 +1,18 @@
-var theName = document.querySelector(".nameTextBox")
-var theGreetBtn = document.querySelector(".greetNameBtn")
-var myGreetings = document.querySelector('.myGreetings')
+var theName = document.querySelector(".nameTextBox");
+var theGreetBtn = document.querySelector(".greetNameBtn");
+var myGreetings = document.querySelector('.myGreetings');
+var greetRadios = document.querySelector('.languageRadioName');
+var theLanguages = document.querySelector('.languages')
 var theGreetMe = theGreetExercise();
 
 function greetMe(){
+
     var nameText = theName.value;
+ // var nameRadio = document.querySelector("input[name='languageRadio']:checked");
+   // theGreetMe.greetingsLanguage(nameRadio);
     theGreetMe.greet(nameText);
-    myGreetings.innerHTML = theGreetMe.greet(nameText)
+    myGreetings.innerHTML = theGreetMe.greet(nameText);
+// theLanguages.innerHTML = theGreetMe.greetingsLanguage(nameRadio);
+
 }
 theGreetBtn.addEventListener('click', greetMe)
